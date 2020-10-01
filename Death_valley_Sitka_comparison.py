db6400596ab648e3d1b6dffea1f4883da3ba4f04
McIntyre <Scott_McIntyre1@baylor.edu>
2020-09-30 21:00:59 -0500

    final changes

--- Death_valley_Sitka_comparison.py
+++ Death_valley_Sitka_comparison.py
@@ -11,18 +11,21 @@ csv_death = csv.reader(open_death, delimiter=",")
 
 csv_sitka = csv.reader(open_sitka, delimiter=",")
 
-header_row = next(csv_death)
+header_death = next(csv_death)
 
-header_row = next(csv_sitka)
+header_sitka = next(csv_sitka)
 
 
-print(header_row)
-'''
-for index, column_header in enumerate(header_row):
-    print(index,column_header)
-    if 5 = TMAX
 
-'''
+
+for index, column_header in enumerate(header_death):
+    if column_header == "TMIN":
+        l = index
+    elif column_header == "TMAX":
+        h = index
+    elif column_header == "DATE":
+        d = index
+
 
 highs_death = []
 dates_death = []
@@ -39,9 +42,9 @@ lows_sitka = []
 #death valley
 for row in csv_death:
     try:
-        high_death = int(row[4])
-        low_death = int(row[5])
-        current_date_death = datetime.strptime(row[2],'%Y-%m-%d' )
+        high_death = int(row[h])
+        low_death = int(row[l])
+        current_date_death = datetime.strptime(row[d],'%Y-%m-%d' )
     except ValueError:
         print(f"Missing data for {current_date_death}")
     else:
@@ -49,11 +52,19 @@ for row in csv_death:
         lows_death.append(low_death)
         dates_death.append(current_date_death)     #append date to list
 #sitka
+for index, column_header in enumerate(header_sitka):
+    if column_header == "TMIN":
+        l = index
+    elif column_header == "TMAX":
+        h = index
+    elif column_header == "DATE":
+        d = index
+
 for row in csv_sitka:
     try:
-        high_sitka = int(row[5])
-        low_sitka = int(row[6])
-        current_date_sitka = datetime.strptime(row[2],'%Y-%m-%d' )
+        high_sitka = int(row[h])
+        low_sitka = int(row[l])
+        current_date_sitka = datetime.strptime(row[d],'%Y-%m-%d' )
     except ValueError:
         print(f"Missing data for {current_date_sitka}")
     else:
@@ -63,7 +74,7 @@ for row in csv_sitka:
 
 
 
-
+# CHARTS
 import matplotlib.pyplot as plt 
 
 # make subplots
@@ -80,14 +91,12 @@ ax[1].plot(dates_death, lows_death, c="blue", alpha=0.5)
 ax[0].set_title("Temperature comparison between SITKA AIRPORT, AK US  and DEATH VALLEY, CA US \n \n  SITKA AIRPORT")
 
 
-plt.title("SITKA AIRPORT, AK US " , fontsize=16) #change title
+plt.title("SITKA AIRPORT, AK US " , fontsize=16) 
 plt.ylabel("", fontsize=12)
 
-plt.title("DEATH VALLEY, CA US" , fontsize=16) #change title
+plt.title("DEATH VALLEY, CA US" , fontsize=16) 
 plt.xlabel("", fontsize=12)
 
-
-#plt.title("Temperature comparison between SITKA AIRPORT, AK US  and DEATH VALLEY, CA US")
 
 ax[1].fill_between(dates_death, highs_death, lows_death, facecolor= 'blue', alpha=0.1)
 #plt.ylabel("Temperature (F)", fontsize=12)
